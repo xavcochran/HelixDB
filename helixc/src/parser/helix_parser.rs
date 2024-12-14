29a55f1ff69dae7e5f1de7a8798039c7d2cc070b
Gand4lf <Georgejamescurtis@hotmail.co.uk>
2024-12-14 15:36:37 +0000

    Types done for AST.

--- helixc/src/parser/helix_parser.rs
+++ helixc/src/parser/helix_parser.rs
@@ -1,10 +1,62 @@
-use pest::error::Error;
-use pest::iterators::{Pair, Pairs};
 use pest::Parser;
-pub struct HelixParser;
+use pest_derive::Parser;
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
+pub struct HelixParser;
+
+#[derive(Debug)]
+pub struct Source {
+    pub schemas: Vec<(NodeSchema, EdgeSchema)>,
+    pub queries: Vec<Query>,
+}
+
+#[derive(Debug)]
+pub struct NodeSchema {
+    pub name: String,
+    pub properties: Vec<Field>,
+}
+
+#[derive(Debug)]
+pub struct EdgeSchema {
+    pub name: String,
+    pub from: String,
+    pub to: String,
+    pub properties: Vec<Field>,
+}
+
+#[derive(Debug)]
+pub struct Field {
+    pub name: String,
+    pub data_type: DataType,
+}
+
+#[derive(Debug)]
+pub enum DataType {
+    Number,
+    String,
+    Boolean,
+}
+
+#[derive(Debug)]
+pub struct Query {
+    pub name: String,
+    pub body: query_body,
+    pub return_type: String,
+}
+
+#[derive(Debug)]
+pub enum ElementType {
+    V,
+    E,
+}
+
+#[derive(Debug)]
+pub struct query_body {
+    pub assignment: String,
+    pub element_type: ElementType,
+    pub traversal: Vec<String>,
+}
 
 impl HelixParser {}
 
